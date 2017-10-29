cc21730ed1c8fa51aa31e2e8f706cdeff37fdd9d
HideakiTouhara <hideee6723@gmail.com>
2017-10-29 19:37:13 +0900

    Use light

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuraiton)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +29,10 @@ class ViewController: UIViewController {
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/2)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0,0)
+        node.position = SCNVector3(0,0,-0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
